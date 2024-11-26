0d30d6381270193f9497fd89961fd2e712773d81
cpminipc <cpierres@hotmail.com>
2024-11-26 21:49:56 +0100

    cp02-24-BookService - Implémentation de l'interface du Service avec @Service

--- src/main/java/guru/springframework/spring6webapp/services/BookService.java
+++ src/main/java/guru/springframework/spring6webapp/services/BookService.java
@@ -2,6 +2,6 @@ package guru.springframework.spring6webapp.services;
 
 import guru.springframework.spring6webapp.domain.Book;
 
-public interface BookService {
+public interface BookService  {
     Iterable<Book> findAll();
 }
